4cc24825cf49e2e585d7cf3e5f74613f7ff395d8
jaslevy <jasplevy@gmail.com>
2024-09-04 13:23:01 -0400

    made pylint changes and added minitorch init docstring

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,3 +1,4 @@
+"""Minitorch package."""
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .module import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
