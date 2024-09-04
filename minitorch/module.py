4b11846fc7f1a030e9efeec426dff63cf917e7e2
jaslevy <jasplevy@gmail.com>
2024-09-04 12:26:11 -0400

    module.py first try

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,14 +31,15 @@ class Module:
 
     def train(self) -> None:
         """Set the `training` flag of this and descendent to true."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for module in self.modules():
+            module.train()
 
     def eval(self) -> None:
         """Set the `training` flag of this and descendent to false."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
-
+        self.training = False
+        for module in self.modules():
+            module.eval()
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """Collect all the parameters of this module and its descendents.
 
@@ -47,13 +48,22 @@ class Module:
             The name and `Parameter` of each ancestor parameter.
 
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        params = []
+        for name, param in self._parameters.items():
+            params.append((name, param))
+        for name, module in self._modules.items():
+            for n, p in module.named_parameters():
+                params.append((".".join([name, n]), p))
+        return params
 
     def parameters(self) -> Sequence[Parameter]:
         """Enumerate over all the parameters of this module and its descendents."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        params = []
+        for param in self._parameters.values():
+            params.append(param)
+        for module in self._modules.values():
+            params.extend(module.parameters())
+        return params
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """Manually add a parameter. Useful helper for scalar parameters.
